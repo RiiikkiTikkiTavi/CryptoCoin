82bb72c94a373842ed18713de9d35ae0fd1d81fe
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-07-17 21:09:56 +0300

    Создана страница списка криптовалют, добавлена svg-картинка

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const CryptoCurrenciesListApp());
@@ -14,7 +15,16 @@ class CryptoCurrenciesListApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: false,
         primarySwatch: Colors.yellow, // дефолтный цвет, если не задан другой
-        scaffoldBackgroundColor: Colors.black87,
+        dividerColor: Colors.white24, // цвет разделителя списка
+        scaffoldBackgroundColor: Colors.black87, // цвет фона
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black87,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
             color: Colors.white,
@@ -27,6 +37,7 @@ class CryptoCurrenciesListApp extends StatelessWidget {
             fontSize: 14,
           ),
         ),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white24),
       ),
       home: const MyHomePage(),
     );
@@ -54,21 +65,28 @@ class _MyHomePageState extends State<MyHomePage> {
     final theme = Theme.of(context); // обращение к общей теме
     return Scaffold(
       appBar: AppBar(title: const Text('Crypto Currencies List')),
-      body: ListView.builder(
+      body: ListView.separated(
         itemCount: 50,
+        separatorBuilder: (context, index) => const Divider(),
         itemBuilder: (context, i) => ListTile(
+          leading: SvgPicture.asset(
+            'assets/svg/bitcoin.svg',
+            height: 30,
+            width: 30,
+          ),
           title: Text(
             'Bitcoin',
             style: theme.textTheme.bodyMedium,
           ), // применение стиля bodyMedium к тексту
           subtitle: Text('100000\$', style: theme.textTheme.labelSmall),
+          trailing: const Icon(Icons.arrow_forward),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
