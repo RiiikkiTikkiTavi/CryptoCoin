2043b2963cb5b3a7ab6bbaac3253d1a83f54777c
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-07-24 22:02:10 +0300

    Добавлено сравнение объектов с помощью equatable, чтобы избежать лишних перерисовок

--- lib/features/crypto_list/bloc/crypto_list_event.dart
+++ lib/features/crypto_list/bloc/crypto_list_event.dart
@@ -3,9 +3,13 @@
 
 part of 'crypto_list_bloc.dart';
 
-class CryptoListEvent {}
+// базовый event
+abstract class CryptoListEvent extends Equatable {}
 
 class LoadCryptoList extends CryptoListEvent {
   final Completer? completer;
   LoadCryptoList({this.completer});
+
+  @override
+  List<Object?> get props => [completer];
 }
--- lib/features/crypto_list/bloc/crypto_list_state.dart
+++ lib/features/crypto_list/bloc/crypto_list_state.dart
@@ -4,20 +4,32 @@
 
 part of 'crypto_list_bloc.dart';
 
-class CryptoListState {}
+abstract class CryptoListState extends Equatable {}
 
-class CryptoListInitial extends CryptoListState {}
+class CryptoListInitial extends CryptoListState {
+  @override
+  List<Object?> get props => [];
+}
 
-class CryptoListLoading extends CryptoListState {}
+class CryptoListLoading extends CryptoListState {
+  @override
+  List<Object?> get props => [];
+}
 
 // вызывается, когда экран с криптой готов, валюты загрузились
 class CryptoListLoaded extends CryptoListState {
   final List<CryptoCoin> coinList;
   CryptoListLoaded({required this.coinList});
+
+  @override
+  List<Object?> get props => [coinList];
 }
 
 // вызывается, когда возникает ошибка
 class CryptoListLoadingFailure extends CryptoListState {
   final Object? exception;
   CryptoListLoadingFailure({this.exception});
+
+  @override
+  List<Object?> get props => [exception];
 }
