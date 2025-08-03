a36af11dfc28fd201dd684d196b69b2b14c91171
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-08-03 22:04:42 +0300

    Поправлен route

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,18 +1,7 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:crypto_coins/features/crypto_coin/crypto_coin.dart';
 import 'package:crypto_coins/features/crypto_list/crypto_list.dart';
 
-// final routes = {
-//   '/': (context) => const CryptoListScreen(),
-//   '/coin': (context) => const CryptoCoinScreen(),
-// };
-
-part 'router.gr.dart';
-
-@AutoRouterConfig()
-class AppRouter extends _$AppRouter {
-  @override
-  List<AutoRoute> get routes => [
-    /// routes go here
-  ];
-}
+final routes = {
+  '/': (context) => const CryptoListScreen(),
+  '/coin': (context) => const CryptoCoinScreen(),
+};
